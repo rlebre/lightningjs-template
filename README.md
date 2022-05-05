e171312a7fe51f113b3bb9f551975e9010313a35
Rui Lebre <ruilebre@ua.pt>
2022-05-05 11:14:04 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,26 @@
+# LightningJS
+
+## com.ruilebre.template
+
+### Getting started
+
+```bash
+yarn
+```
+
+#### Developing the App
+
+During development you can use the **watcher** functionality of webpack.
+
+- run `yarn dev` to start webpack dev server
+
+#### Building the App
+
+- run `yarn build` to bundle the files using webpack
+
+#### Documentation
+
+> Before you follow the steps below, make sure you have the
+> [Lightning-CLI](https://rdkcentral.github.io/Lightning-CLI/#/) installed _globally_ only your system
+
+Use `lng docs` to open up the Lightning-SDK documentation.
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'public/index.html'
+  }),
+  new CopyWebpackPlugin({
+    patterns: [
+      { from: path.resolve(__dirname, './public/images'), to: 'images' },
+      { from: path.resolve(__dirname, './public/fonts'), to: 'fonts' }
+    ]
+  })
+];
+
+const config = {
+  entry: ['./src/index.js'],
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    open: true,
+    host: '0.0.0.0',
+    port: process.env.PORT | 3000,
+    static: path.resolve(__dirname, 'public')
+  },
+  plugins,
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
+
+module.exports = config;
